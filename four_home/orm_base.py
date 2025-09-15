0bc10ec692a2a4f409d38e6069b045a89df0fd0c
notsimplewords-cell <notsimplewords@gmail.com>
2025-09-15 22:39:19 +0500

    Новая версия
    улучшена версия проекта
    теперь оно хотябы создает базу данных +исп глб пит

--- four_home/orm_base.py
+++ four_home/orm_base.py
@@ -1,25 +1,16 @@
-from sqlmodel import SQLModel, Field, Session, select, func
-from datetime import date 
-from typing import Optional
+from sqlmodel import SQLModel, Field, Session, create_engine, Relationship
+from datetime import date
+from typing import Optional, List
 
-class patients(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class Patient(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     username: str
     email: str = Field(index=True, unique=True)
     birth_date: Optional[date] = None
     hashed_password: str
 
-class doctor(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    username: str
-    email: str = Field(index=True, unique=True)
-    birth_date: Optional[date] = None
-    specialization: str
-    experience: int
-    hashed_password: str
-
-class doctor(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class Doctor(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     username: str
     email: str = Field(index=True, unique=True)
     birth_date: Optional[date] = None
@@ -27,7 +18,7 @@ class doctor(SQLModel, table=True):
     experience: int
     hashed_password: str
 
-class visit(SQLModel, table=True):
+class Visit(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     patient_id: int = Field(foreign_key="patient.id")
     doctor_id: int = Field(foreign_key="doctor.id")
@@ -35,9 +26,19 @@ class visit(SQLModel, table=True):
     complaints: Optional[str] = None
     diagnosis: Optional[str] = None
 
-class prescription(SQLModel, table=True):
+class Prescription(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     visit_id: int = Field(foreign_key="visit.id")
     medicine: str
     dose: str
     instructions: Optional[str] = None
+
+
+
+sqlite_file_name = "four_home/dentistry.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+# создаём таблицы
+SQLModel.metadata.create_all(engine)
